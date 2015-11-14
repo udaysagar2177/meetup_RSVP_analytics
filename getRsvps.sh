23677743a91f8def91d38c6f972acb6e49d94753
udaysagar2177 <udaysagar.2177@gmail.com>
2015-11-13 19:58:54 -0800

    Corrected getRsvps.sh

--- getRsvps.sh
+++ getRsvps.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-while true
-do 
 count=0
-count=$(( $count + 1 ))
-echo "Curl Attempt ${count}" >> /opt/meetup_data1_log.txt
-start=$(date)
-echo "Start time: ${start}" >> /opt/meetup_data1_log.txt
-curl http://stream.meetup.com/2/rsvps >> /opt/meetup_data1.json
-end=$(date)
-echo "End time: ${end}" >> /opt/meetup_data1_log.txt
+datafilename="meetup_data1.txt"
+logfilename="meetup_data1_log.txt"
+
+while true
+do
+  count=$(( $count + 1 ))
+  echo "Curl Attempt ${count}" >> /opt/$logfilename
+  start=$(date)
+  echo "Start time: ${start}" >> /opt/$logfilename
+  curl http://stream.meetup.com/2/rsvps >> /opt/$datafilename
+  end=$(date)
+  echo "End time: ${end}" >> /opt/$logfilename
 done
