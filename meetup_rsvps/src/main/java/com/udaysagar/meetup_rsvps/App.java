f1a7770d8ade59c8c2ee8b3c3b68bbddd4bf32ba
udaysagar2177 <udaysagar.2177@gmail.com>
2015-11-14 18:37:25 -0800

    Get RSVPs with value = yes

--- meetup_rsvps/src/main/java/com/udaysagar/meetup_rsvps/App.java
+++ meetup_rsvps/src/main/java/com/udaysagar/meetup_rsvps/App.java
@@ -1,26 +1,68 @@
 package com.udaysagar.meetup_rsvps;
 
+import java.util.Map;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
-public class App 
-{
+public class App {
 	public static void main(String[] args) throws Exception {
 		Logger.getLogger("org").setLevel(Level.OFF);
 		Logger.getLogger("akka").setLevel(Level.OFF);
-	    
-		SparkConf conf = new SparkConf().setMaster("local[4]").setAppName("Meetup RSVPs");
-	    JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
+
+		String masterURL = "local[4]";
+		String appName = "Meetup RSVPs";
+		String URL = "http://stream.meetup.com/2/rsvps";
+		int durationInSeconds = 5;
+
+		SparkConf conf = new SparkConf().setMaster(masterURL).setAppName(appName);
+		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(durationInSeconds));
+
+		JavaDStream <Map> rsvps = jssc.receiverStream(new JavaCustomReceiver(URL));
+
+		// Get the RSVP with value = yes
+		JavaDStream <Map> rsvpsYes = rsvps.filter(new Function<Map, Boolean>(){
+			public Boolean call(Map rsvp) {
+				if (rsvp.get("response").toString().compareTo("yes") == 0){
+					return true;
+				}
+				return false;
+			}
+		});
 		
-	    JavaDStream<String> customReceiverStream = jssc.receiverStream(new JavaCustomReceiver("http://stream.meetup.com/2/rsvps"));
-	    
-	    customReceiverStream.print();
-	    
-	    jssc.start();             // Start the computation
-	    jssc.awaitTermination();
+		/*
+		 * Get the number of RSVP with value = yes
+		 */
+		JavaDStream <Long> rsvpsYesCount = rsvps.filter(new Function<Map, Boolean>(){
+			public Boolean call(Map rsvp) {
+				if (rsvp.get("response").toString().compareTo("yes") == 0){
+					return true;
+				}
+				return false;
+			}
+		}).count();
+		
+		
+		/*
+		 * Get the number of RSVP with value = no
+		 */
+		JavaDStream <Long> rsvpsNoCount = rsvps.filter(new Function<Map, Boolean>(){
+			public Boolean call(Map rsvp) {
+				if (rsvp.get("response").toString().compareTo("no") == 0){
+					return true;
+				}
+				return false;
+			}
+		}).count();
+		
+		rsvpsYes.print();
+		
+		jssc.start(); // Start the computation
+		jssc.awaitTermination();
 	}
 }
--- meetup_rsvps/src/main/java/com/udaysagar/meetup_rsvps/JavaCustomReceiver.java
+++ meetup_rsvps/src/main/java/com/udaysagar/meetup_rsvps/JavaCustomReceiver.java
@@ -5,62 +5,69 @@ import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.receiver.Receiver;
 
-public class JavaCustomReceiver extends Receiver<String> {
+import com.codesnippets4all.json.parsers.JSONParser;
+import com.codesnippets4all.json.parsers.JsonParserFactory;
 
-	  String host = null;
 
-	  public JavaCustomReceiver(String host_) {
-	    super(StorageLevel.MEMORY_AND_DISK_2());
-	    host = host_;
-	  }
+public class JavaCustomReceiver extends Receiver < Map > {
 
-	  public void onStart() {
-	    // Start the thread that receives data over a connection
-	    new Thread()  {
-	      @Override public void run() {
-	        receive();
-	      }
-	    }.start();
-	  }
+	String host = null;
 
-	  public void onStop() {
-	    // There is nothing much to do as the thread calling receive()
-	    // is designed to stop by itself if isStopped() returns false
-	  }
-
-	  /** Create a socket connection and receive data until receiver is stopped */
-	  private void receive() {
-	   
-	    String userInput = null;
-
-	    try {
-	      // connect to the server
-	      URLConnection connection = new URL("http://stream.meetup.com/2/rsvps/").openConnection();;
-	      connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-	      connection.connect();
-
-	      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-	      // Until stopped or connection broken continue reading
-	      while (!isStopped() && (userInput = reader.readLine()) != null) {
-	        System.out.println("Received data '" + userInput + "'");
-	        store(userInput);
-	      }
-	      reader.close();
-	      
-
-	      // Restart in an attempt to connect again when server is active again
-	      restart("Trying to connect again");
-	    } catch(ConnectException ce) {
-	      // restart if could not connect to server
-	      restart("Could not connect", ce);
-	    } catch(Throwable t) {
-	      // restart if there is any other error
-	      restart("Error receiving data", t);
-	    }
-	  }
+	public JavaCustomReceiver(String host_) {
+		super(StorageLevel.MEMORY_AND_DISK_2());
+		host = host_;
 	}
+
+	public void onStart() {
+		// Start the thread that receives data over a connection
+		new Thread() {@Override public void run() {
+				receive();
+			}
+		}.start();
+	}
+
+	public void onStop() {
+		// There is nothing much to do as the thread calling receive()
+		// is designed to stop by itself if isStopped() returns false
+	}
+
+	/** Create a socket connection and receive data until receiver is stopped */
+	private void receive() {
+
+		String userInput = null;
+
+		try {
+			// connect to the server
+			URLConnection connection = new URL("http://stream.meetup.com/2/rsvps/").openConnection();;
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			connection.connect();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			// Until stopped or connection broken continue reading
+			while (!isStopped() && (userInput = reader.readLine()) != null) {
+				//System.out.println("Received data '" + userInput + "'");
+				JsonParserFactory factory = JsonParserFactory.getInstance();
+				JSONParser parser = factory.newJsonParser();
+				Map jsonData = parser.parseJson(userInput);
+				store(jsonData);
+			}
+			reader.close();
+
+
+			// Restart in an attempt to connect again when server is active again
+			restart("Trying to connect again");
+		} catch (ConnectException ce) {
+			// restart if could not connect to server
+			restart("Could not connect", ce);
+		} catch (Throwable t) {
+			// restart if there is any other error
+			restart("Error receiving data", t);
+		}
+	}
+}
